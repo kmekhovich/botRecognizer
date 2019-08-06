ff2c888c8d3b911b9e88569de9206edf3e40f43d
OS-M <manalog44@gmail.com>
2019-08-06 13:33:52 +0300

    Set to eval AFTER loading the model

--- convMain.py
+++ convMain.py
@@ -22,19 +22,18 @@ dataset = ImageDataset(data_path, train=True, seed=seed, aug_rate=2, load_first_
 dataset.split(0.7)
 dataset.validation_to_numpy()
 dataset.shuffle_all(seed)
-print(dataset.train_answers)
 
 torch.cuda.current_device()
 net = Network(dataset=dataset, learning_rate=0.00001, enable_tb=True, device="cuda:0", weight_decay=0.001,
               output_dims=dataset.n_classes)
-net.set_train()
 # print(net.net)
-net.epoch = 0  # int(input().split()[0])
+net.epoch = 0  # int(input().split()0])
 curr_model_path = model_path + "model_" + str(net.epoch) + ".pt"
 print(curr_model_path)
 if os.path.isfile(curr_model_path):
     net.load_model(curr_model_path)
     print("Model loaded")
+net.set_train()
 
 net.train(savepath=model_path, train_start=-1, train_stop=-1, validation_start=-1,
           validation_stop=-1, validation_batch_size=512, batch_size=32,
