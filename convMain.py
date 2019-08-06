d1440fe1aa69b8890acbd139dd78dded6fd589f6
OS-M <manalog44@gmail.com>
2019-08-06 12:58:30 +0300

    Some changes to train net more easily

--- convMain.py
+++ convMain.py
@@ -8,32 +8,33 @@ from PIL import Image
 
 print("Using convNet Version", Network.__version__)
 
-# data_path = "./drive/My Drive/Images.zip"
-# model_path = "./drive/My Drive/models_dog/"
+data_path = "./drive/My Drive/Images.zip"
+model_path = "./drive/My Drive/models_dog/"
 
-data_path = "./Images.zip"
-model_path = "./models/"
+# data_path = "./Images.zip"
+# model_path = "./models/"
 
-dataset = ImageDataset(data_path, train=True, seed=341, aug_rate=3, load_first_n=9999)
-dataset.split(0.85)
+seed = time.time() % 1000
+seed = 421
+print("SEED", seed)
+
+dataset = ImageDataset(data_path, train=True, seed=seed, aug_rate=2, load_first_n=9999)
+dataset.split(0.7)
 dataset.validation_to_numpy()
-dataset.shuffle_all(643)
+dataset.shuffle_all(seed)
 print(dataset.train_answers)
 
 torch.cuda.current_device()
-net = Network(dataset=dataset, learning_rate=0.0001, enable_tb=True, device="cuda:0", weight_decay=0.00001)
+net = Network(dataset=dataset, learning_rate=0.00001, enable_tb=True, device="cuda:0", weight_decay=0.001,
+              output_dims=dataset.n_classes)
 net.set_train()
 # print(net.net)
-net.epoch = 0# int(input().split()[0])
+net.epoch = 0  # int(input().split()[0])
 curr_model_path = model_path + "model_" + str(net.epoch) + ".pt"
 print(curr_model_path)
 if os.path.isfile(curr_model_path):
     net.load_model(curr_model_path)
     print("Model loaded")
-
-seed = time.time() % 1000
-seed = 600
-print("SEED", seed)
 
 net.train(savepath=model_path, train_start=-1, train_stop=-1, validation_start=-1,
           validation_stop=-1, validation_batch_size=512, batch_size=32,
